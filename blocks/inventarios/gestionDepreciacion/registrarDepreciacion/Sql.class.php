f35dbc1962c8a18fbeee071fc58ea03a1813081c
violetas <violetas@s242028c7g.udistritaloas.edu.co>
2015-03-11 20:05:20 -0500

    Módulo Depreciación - 2 parte

---
+++ blocks/inventarios/gestionDepreciacion/registrarDepreciacion/Sql.class.php
@@ -0,0 +1,255 @@
+<?php
+
+namespace inventarios\gestionDepreciacion\registrarDepreciacion;
+
+if (!isset($GLOBALS ["autorizado"])) {
+    include ("../index.php");
+    exit();
+}
+
+include_once ("core/manager/Configurador.class.php");
+include_once ("core/connection/Sql.class.php");
+
+// Para evitar redefiniciones de clases el nombre de la clase del archivo sqle debe corresponder al nombre del bloque
+// en camel case precedida por la palabra sql
+class Sql extends \Sql {
+
+    var $miConfigurador;
+
+    function __construct() {
+        $this->miConfigurador = \Configurador::singleton();
+    }
+
+    function getCadenaSql($tipo, $variable = "") {
+        /**
+         * 1.
+         * Revisar las variables para evitar SQL Injection
+         */
+        $prefijo = $this->miConfigurador->getVariableConfiguracion("prefijo");
+        $idSesion = $this->miConfigurador->getVariableConfiguracion("id_sesion");
+
+        switch ($tipo) {
+            /**
+             * Clausulas específicas
+             */
+            case "buscarUsuario" :
+                $cadenaSql = "SELECT ";
+                $cadenaSql .= "FECHA_CREACION, ";
+                $cadenaSql .= "PRIMER_NOMBRE ";
+                $cadenaSql .= "FROM ";
+                $cadenaSql .= "USUARIOS ";
+                $cadenaSql .= "WHERE ";
+                $cadenaSql .= "`PRIMER_NOMBRE` ='" . $variable . "' ";
+                break;
+
+            case "insertarRegistro" :
+                $cadenaSql = "INSERT INTO ";
+                $cadenaSql .= $prefijo . "registradoConferencia ";
+                $cadenaSql .= "( ";
+                $cadenaSql .= "`idRegistrado`, ";
+                $cadenaSql .= "`nombre`, ";
+                $cadenaSql .= "`apellido`, ";
+                $cadenaSql .= "`identificacion`, ";
+                $cadenaSql .= "`codigo`, ";
+                $cadenaSql .= "`correo`, ";
+                $cadenaSql .= "`tipo`, ";
+                $cadenaSql .= "`fecha` ";
+                $cadenaSql .= ") ";
+                $cadenaSql .= "VALUES ";
+                $cadenaSql .= "( ";
+                $cadenaSql .= "NULL, ";
+                $cadenaSql .= "'" . $variable ['nombre'] . "', ";
+                $cadenaSql .= "'" . $variable ['apellido'] . "', ";
+                $cadenaSql .= "'" . $variable ['identificacion'] . "', ";
+                $cadenaSql .= "'" . $variable ['codigo'] . "', ";
+                $cadenaSql .= "'" . $variable ['correo'] . "', ";
+                $cadenaSql .= "'0', ";
+                $cadenaSql .= "'" . time() . "' ";
+                $cadenaSql .= ")";
+                break;
+
+            case "actualizarRegistro" :
+                $cadenaSql = "UPDATE ";
+                $cadenaSql .= $prefijo . "conductor ";
+                $cadenaSql .= "SET ";
+                $cadenaSql .= "`nombre` = '" . $variable ["nombre"] . "', ";
+                $cadenaSql .= "`apellido` = '" . $variable ["apellido"] . "', ";
+                $cadenaSql .= "`identificacion` = '" . $variable ["identificacion"] . "', ";
+                $cadenaSql .= "`telefono` = '" . $variable ["telefono"] . "' ";
+                $cadenaSql .= "WHERE ";
+                $cadenaSql .= "`idConductor` =" . $_REQUEST ["registro"] . " ";
+                break;
+
+            /**
+             * Clausulas genéricas.
+             * se espera que estén en todos los formularios
+             * que utilicen esta plantilla
+             */
+            case "iniciarTransaccion" :
+                $cadenaSql = "START TRANSACTION";
+                break;
+
+            case "finalizarTransaccion" :
+                $cadenaSql = "COMMIT";
+                break;
+
+            case "cancelarTransaccion" :
+                $cadenaSql = "ROLLBACK";
+                break;
+
+            case "eliminarTemp" :
+                $cadenaSql = "DELETE ";
+                $cadenaSql .= "FROM ";
+                $cadenaSql .= $prefijo . "tempFormulario ";
+                $cadenaSql .= "WHERE ";
+                $cadenaSql .= "id_sesion = '" . $variable . "' ";
+                break;
+
+            case "insertarTemp" :
+                $cadenaSql = "INSERT INTO ";
+                $cadenaSql .= $prefijo . "tempFormulario ";
+                $cadenaSql .= "( ";
+                $cadenaSql .= "id_sesion, ";
+                $cadenaSql .= "formulario, ";
+                $cadenaSql .= "campo, ";
+                $cadenaSql .= "valor, ";
+                $cadenaSql .= "fecha ";
+                $cadenaSql .= ") ";
+                $cadenaSql .= "VALUES ";
+
+                foreach ($_REQUEST as $clave => $valor) {
+                    $cadenaSql .= "( ";
+                    $cadenaSql .= "'" . $idSesion . "', ";
+                    $cadenaSql .= "'" . $variable ['formulario'] . "', ";
+                    $cadenaSql .= "'" . $clave . "', ";
+                    $cadenaSql .= "'" . $valor . "', ";
+                    $cadenaSql .= "'" . $variable ['fecha'] . "' ";
+                    $cadenaSql .= "),";
+                }
+
+                $cadenaSql = substr($cadenaSql, 0, (strlen($cadenaSql) - 1));
+                break;
+
+            case "rescatarTemp" :
+                $cadenaSql = "SELECT ";
+                $cadenaSql .= "id_sesion, ";
+                $cadenaSql .= "formulario, ";
+                $cadenaSql .= "campo, ";
+                $cadenaSql .= "valor, ";
+                $cadenaSql .= "fecha ";
+                $cadenaSql .= "FROM ";
+                $cadenaSql .= $prefijo . "tempFormulario ";
+                $cadenaSql .= "WHERE ";
+                $cadenaSql .= "id_sesion='" . $idSesion . "'";
+                break;
+
+//-----------------------------** Cláusulas del caso de uso**----------------------------------//
+
+            case "consultar_nivel_inventario" :
+                $cadenaSql = "SELECT codigo,(codigo||' - '||nombre) AS nivel ";
+                $cadenaSql .= "FROM catalogo_elemento ;";
+                break;
+
+            case "consultar_grupo_contable" :
+                $cadenaSql = "SELECT grupo_numcuenta, (grupo_numcuenta||' - '||grupo_descripcion) as grupo,grupo_mesdepreciacion as meses_depreciar ";
+                $cadenaSql.= " FROM grupo_contable  ";
+                $cadenaSql.= " WHERE grupo_estado=TRUE";
+                break;
+
+            case "consultar_meses" :
+                $cadenaSql = "SELECT grupo_mesdepreciacion ";
+                $cadenaSql.= " FROM grupo_contable  ";
+                $cadenaSql.= " WHERE grupo_estado=TRUE ";
+                $cadenaSql.= " AND grupo_numcuenta='" . $variable . "'; ";
+                break;
+
+            case "consultarElementos" :
+                $cadenaSql = "SELECT  ";
+                $cadenaSql.= " id_elemento,  ";
+                $cadenaSql.= " nivel,  ";
+                $cadenaSql.= " unidad, ";
+                $cadenaSql.= " cantidad,  ";
+                $cadenaSql.= " marca,  ";
+                $cadenaSql.= " serie, ";
+                $cadenaSql.= " valor, ";
+                $cadenaSql.= " subtotal_sin_iva,  ";
+                $cadenaSql.= " total_iva, ";
+                $cadenaSql.= " total_iva_con";
+                $cadenaSql.= " FROM elemento, salida ";
+                $cadenaSql.= " WHERE elemento.estado=TRUE ";
+                $cadenaSql.= " AND salida.id_entrada=elemento.id_entrada ";
+                $cadenaSql.= " AND nivel='" . $variable . "' ";
+                $cadenaSql.= " ORDER BY id_elemento ASC ";
+                break;
+
+            case "consultarElemento_especifico" :
+                $cadenaSql = "SELECT  ";
+                $cadenaSql.= " id_elemento,  ";
+                $cadenaSql.= " nivel,  ";
+                $cadenaSql.= " unidad, ";
+                $cadenaSql.= " cantidad,  ";
+                $cadenaSql.= " marca,  ";
+                $cadenaSql.= " serie, ";
+                $cadenaSql.= " valor, ";
+                $cadenaSql.= " subtotal_sin_iva,  ";
+                $cadenaSql.= " total_iva, ";
+                $cadenaSql.= " total_iva_con, ";
+                $cadenaSql.= " salida.fecha ";
+                $cadenaSql.= " FROM elemento, salida ";
+                $cadenaSql.= " WHERE elemento.estado=TRUE ";
+                $cadenaSql.= " AND salida.id_entrada=elemento.id_entrada ";
+                $cadenaSql.= " AND id_elemento='" . $variable . "' ";
+                $cadenaSql.= " ORDER BY id_elemento ASC ";
+                break;
+
+            case "registrarDepreciacion":
+                $cadenaSql = " INSERT INTO registro_depreciacion( ";
+                $cadenaSql.= " dep_idelementogen, ";
+                $cadenaSql.= " dep_grupocontable,  ";
+                $cadenaSql.= " dep_meses,  ";
+                $cadenaSql.= " dep_fechasalida,  ";
+                $cadenaSql.= " dep_fechacorte,  ";
+                $cadenaSql.= " dep_cantidad,  ";
+                $cadenaSql.= " dep_precio,  ";
+                $cadenaSql.= " dep_valorhistorico,  ";
+                $cadenaSql.= " dep_valorajustado,  ";
+                $cadenaSql.= " dep_cuota,  ";
+                $cadenaSql.= " dep_periodo,  ";
+                $cadenaSql.= " dep_depacumulada, ";
+                $cadenaSql.= " dep_circular56,";
+                $cadenaSql.= " dep_cuotainflacion, ";
+                $cadenaSql.= " dep_apicacumulada, ";
+                $cadenaSql.= " dep_circulardeprecia, ";
+                $cadenaSql.= " dep_libros, ";
+                $cadenaSql.= " dep_estado, ";
+                $cadenaSql.= " dep_registro) ";
+                $cadenaSql.= " VALUES ( ";
+                $cadenaSql.= "'".$variable['elemento_general']."', ";
+                $cadenaSql.= "'".$variable['grupo_contable']."', ";
+                $cadenaSql.= "'".$variable['meses_depreciar']."', ";
+                $cadenaSql.= "'".$variable['fechaSalida']."', ";
+                $cadenaSql.= "'".$variable['fechaCorte']."', ";
+                $cadenaSql.= "'".$variable['cantidad']."', ";
+                $cadenaSql.= "'".$variable['precio']."', ";
+                $cadenaSql.= "'".$variable['valor_historico']."', ";
+                $cadenaSql.= "'".$variable['valor_ajustado']."', ";
+                $cadenaSql.= "'".$variable['cuota']."', ";
+                $cadenaSql.= "'".$variable['periodos_fecha']."', ";
+                $cadenaSql.= "'".$variable['depreciacion_acumulada']."', ";
+                $cadenaSql.= "'".$variable['circular_56']."', ";
+                $cadenaSql.= "'".$variable['cuota_inflacion']."', ";
+                $cadenaSql.= "'".$variable['api_acumulada']."', ";
+                $cadenaSql.= "'".$variable['circular_depreciacion']."', ";
+                $cadenaSql.= "'".$variable['valor_libros']."', ";
+                $cadenaSql.= "'".$variable['estado']."', ";
+                $cadenaSql.= "'".$variable['fregistro']."') ";
+                $cadenaSql.= " ;";
+                break;
+            /*             * ***************** */
+        }
+        return $cadenaSql;
+    }
+
+}
+
+?>
---
+++ blocks/inventarios/gestionDepreciacion/registrarDepreciacion/funcion/registrarDepreciacion.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace inventarios\gestionDepreciacion\registrarDepreciacion\funcion;
+
+use inventarios\gestionDepreciacion\registrarDepreciacion\funcion\redireccion;
+
+include_once ('redireccionar.php');
+if (!isset($GLOBALS ["autorizado"])) {
+    include ("../index.php");
+    exit();
+}
+
+class RegistradorDepreciacion {
+
+    var $miConfigurador;
+    var $lenguaje;
+    var $miFormulario;
+    var $miFuncion;
+    var $miSql;
+    var $conexion;
+
+    function __construct($lenguaje, $sql, $funcion) {
+        $this->miConfigurador = \Configurador::singleton();
+        $this->miConfigurador->fabricaConexiones->setRecursoDB('principal');
+        $this->lenguaje = $lenguaje;
+        $this->miSql = $sql;
+        $this->miFuncion = $funcion;
+    }
+
+    function procesarFormulario() {
+
+        $fechaActual = date('Y-m-d');
+
+        $esteBloque = $this->miConfigurador->getVariableConfiguracion("esteBloque");
+
+        $rutaBloque = $this->miConfigurador->getVariableConfiguracion("raizDocumento") . "/blocks/inventarios/gestionActa/";
+        $rutaBloque .= $esteBloque ['nombre'];
+        $host = $this->miConfigurador->getVariableConfiguracion("host") . $this->miConfigurador->getVariableConfiguracion("site") . "/blocks/inventarios/gestionActa/" . $esteBloque ['nombre'];
+
+        $conexion = "inventarios";
+        $esteRecursoDB = $this->miConfigurador->fabricaConexiones->getRecursoDB($conexion);
+
+        // Organizar contenido del registro
+        $datos_depreciacion = array(
+            'elemento_general' => (isset($_REQUEST['elemento_general']) ? $_REQUEST['elemento_general'] : ''),
+            'grupo_contable' => (isset($_REQUEST['grupo_contable']) ? $_REQUEST['grupo_contable'] : ''),
+            'meses_depreciar' => (isset($_REQUEST['meses_depreciar']) ? $_REQUEST['meses_depreciar'] : ''),
+            'fechaSalida' => (isset($_REQUEST['fechaSalida']) ? $_REQUEST['fechaSalida'] : ''),
+            'fechaCorte' => (isset($_REQUEST['fechaCorte']) ? $_REQUEST['fechaCorte'] : ''),
+            'cantidad' => (isset($_REQUEST['cantidad']) ? $_REQUEST['cantidad'] : ''),
+            'precio' => number_format((float) (isset($_REQUEST['precio']) ? $_REQUEST['precio'] : ''), 2, '.', ''),
+            'valor_historico' => number_format((float) (isset($_REQUEST['valor_historico']) ? $_REQUEST['valor_historico'] : ''), 2, '.', ''),
+            'valor_ajustado' => number_format((float) (isset($_REQUEST['valor_ajustado']) ? $_REQUEST['valor_ajustado'] : ''), 2, '.', ''),
+            'cuota' => number_format((float) (isset($_REQUEST['cuota']) ? $_REQUEST['cuota'] : ''), 2, '.', ''),
+            'periodos_fecha' => (isset($_REQUEST['periodos_fecha']) ? $_REQUEST['periodos_fecha'] : ''),
+            'depreciacion_acumulada' => number_format((float) (isset($_REQUEST['depreciacion_acumulada']) ? $_REQUEST['depreciacion_acumulada'] : ''), 2, '.', ''),
+            'circular_56' => number_format((float) (isset($_REQUEST['circular_56']) ? $_REQUEST['circular_56'] : ''), 2, '.', ''),
+            'cuota_inflacion' => number_format((float) (isset($_REQUEST['cuota_inflacion']) ? $_REQUEST['cuota_inflacion'] : ''), 2, '.', ''),
+            'api_acumulada' => number_format((float) (isset($_REQUEST['api_acumulada']) ? $_REQUEST['api_acumulada'] : ''), 2, '.', ''),
+            'circular_depreciacion' => number_format((float) (isset($_REQUEST['circular_depreciacion']) ? $_REQUEST['circular_depreciacion'] : ''), 2, '.', ''),
+            'valor_libros' => number_format((float) (isset($_REQUEST['valor_libros']) ? $_REQUEST['valor_libros'] : ''), 2, '.', ''),
+            'estado' => TRUE,
+            'fregistro' => $fechaActual,
+        );
+
+
+        $cadenaSql = $this->miSql->getCadenaSql('registrarDepreciacion', $datos_depreciacion);
+        $registro_depreciacion = $esteRecursoDB->ejecutarAcceso($cadenaSql, "insertar");
+
+
+        $datos = array(
+            'elemento_general' => (isset($_REQUEST['elemento_general']) ? $_REQUEST['elemento_general'] : ''),
+        );
+        //inactivar item para asignar
+        if ($registro_depreciacion == true) {
+            redireccion::redireccionar('inserto', $datos);
+        } else {
+            redireccion::redireccionar('noInserto', $datos);
+        }
+    }
+
+    function resetForm() {
+        foreach ($_REQUEST as $clave => $valor) {
+            if ($clave != 'pagina' && $clave != 'development' && $clave != 'jquery' && $clave != 'tiempo') {
+                unset($_REQUEST [$clave]);
+            }
+        }
+    }
+
+}
+
+$miRegistrador = new RegistradorDepreciacion($this->lenguaje, $this->sql, $this->funcion);
+
+$resultado = $miRegistrador->procesarFormulario();
+?>
