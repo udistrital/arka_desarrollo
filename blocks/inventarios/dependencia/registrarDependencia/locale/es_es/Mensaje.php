da8b1214cf6cb83418fd71b0e9842dac7b59bf2a
violetas <violetas@s242028c7g.udistritaloas.edu.co>
2015-03-24 00:48:31 -0500

    Registro de Dependencia ~ Versión Definitiva y Versión Alternativa no finalizada.

---
+++ blocks/inventarios/dependencia/registrarDependencia/locale/es_es/Mensaje.php
@@ -0,0 +1,75 @@
+<?php
+$this->idioma ["noDefinido"] = "Etiqueta no esta definida en locale/es_es/Mensaje.php";
+
+$this->idioma ["fecha_inicio"] ="Fecha Inicial : ";
+$this->idioma ["fecha_inicioTitulo"] ="Seleccione Fecha Inicial";
+
+$this->idioma ["fecha_final"] ="Fecha Final : ";
+$this->idioma ["fecha_finalTitulo"] ="Seleccione Fecha Final";
+
+$this->idioma ["fecha_recibido"] ="&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspFecha Recibido : ";
+$this->idioma ["fecha_recibidoTitulo"] ="Fecha Recibido";
+
+$this->idioma ["fecha_factura"] ="&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspFecha Factura : ";
+$this->idioma ["fecha_facturaTitulo"] ="Seleccione Fecha Factura";
+
+$this->idioma ["fecha_revision"] ="&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspFecha Revisión : ";
+$this->idioma ["fecha_revisionTitulo"] ="Seleccione Fecha Revisión";
+
+$this->idioma ["numero_orden"] ="Número de Orden: ";
+$this->idioma ["numero_ordenTitulo"] ="Ingrese Número Orden";
+
+$this->idioma ["tipoOrden"] ="Tipo de Orden: ";
+$this->idioma ["tipoOrdenTitulo"] ="Seleccione Tipo Orden";
+
+$this->idioma ["tipoBien"] ="&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspTipo de Bien: ";
+$this->idioma ["tipoBienTitulo"] ="Seleccione Tipo de Bien";
+
+$this->idioma ["tipoAccion"] ="&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspTipo Acción: ";
+$this->idioma ["tipoAccionTitulo"] ="Seleccione Tipo Acción";
+
+$this->idioma ["tipoComprador"] ="&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspComprador: ";
+$this->idioma ["tipoCompradorTitulo"] ="Seleccione Quien Compra";
+
+
+$this->idioma ["idActa"] ="ID Acta: ";
+$this->idioma ["idActaTitulo"] ="Acta Recibido";
+
+$this->idioma ["dependencia"] ="&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspNombre Dependencia :";
+$this->idioma ["dependenciaTitulo"] ="Ingrese Nombre Dependencia";
+
+$this->idioma ["revisor"] ="&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspNombre Revisor :";
+$this->idioma ["revisorTitulo"] ="Ingrese Nombre de Quien Revisó";
+
+$this->idioma ["proveedor"] ="&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspNombre o Razón Social:";
+$this->idioma ["proveedorTitulo"] ="Ingrese Nombre Proveedor";
+
+$this->idioma ["nitproveedor"] ="&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspNIT Proveedor :";
+$this->idioma ["nitproveedorTitulo"] ="Ingrese NIT Proveedor";
+
+$this->idioma ["numFactura"] ="&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspNúmero Factura :";
+$this->idioma ["numFacturaTitulo"] ="Ingrese Número Factura";
+
+$this->idioma ["descripcionFactura"] ="Descripción Factura";
+
+$this->idioma ["observacionesActa"] ="Observaciones: ";
+
+$this->idioma ["aprobacionCheck"] ="&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp¿Aprobado?:";
+$this->idioma ["aprobacionCheckTitulo"] ="Seleccione en caso de ser aprobado";
+
+
+$this->idioma ["botonAceptar"] ="Registrar Acta de Recibido";
+$this->idioma ["botonCancelar"] ="Cancelar";
+$this->idioma ["botonContinuar"] ="Continuar";
+$this->idioma ["botonConsultar"] ="Consultar";
+$this->idioma ["botonRegresar"] ="Regresar";
+
+
+
+
+
+
+///*********************
+
+
+?>
