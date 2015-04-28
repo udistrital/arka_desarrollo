6f45e554096c980a4dd8e76f15fa4fb02e67c094
violetas <violetas@s242028c7g.udistritaloas.edu.co>
2015-04-28 11:36:24 -0500

    Documentos soporte elementos radicacion movimientos

---
+++ blocks/inventarios/gestionElementos/radicacionesElementos/locale/es_es/Mensaje.php
@@ -0,0 +1,34 @@
+<?php
+$this->idioma ["noDefinido"] = "Etiqueta no esta definida en locale/es_es/Mensaje.php";
+
+$this->idioma ["tipo"] = "Tipo Inexistencia : ";
+$this->idioma ["tipoTitulo"] = "Seleccione Tipo Inexistencia";
+
+
+$this->idioma ["documento"] = "Cargue Documento : <br>a Radicar";
+$this->idioma ["documentoTitulo"] = "Seleccione Documento a Radicar ";
+
+// --
+$this->idioma ["vigencia_registro"] = "Vigencia Registro : ";
+$this->idioma ["vigencia_registroTitulo"] = "Seleccione Vigencia Registro";
+
+$this->idioma ["registro"] = "Número Registro : ";
+$this->idioma ["registroTitulo"] = "Seleccione Número Registro";
+
+$this->idioma ["valor_registro"] = "Valor  Registro ($): ";
+$this->idioma ["valor_registroTitulo"] = "Valor Registro Presupuestal";
+
+$this->idioma ["fecha_registro"] = "Fecha Registro : ";
+$this->idioma ["fecha_registroTitulo"] = "Fecha de Registro Presupuestal";
+
+$this->idioma ["valorL_registro"] = "Valor en Letras : ";
+$this->idioma ["valorL_registroTitulo"] = "Valor en Letras  Registro Presupuestal";
+
+// ________________________________________________
+$this->idioma ["botonAceptar"] = "Guardar";
+$this->idioma ["botonCancelar"] = "Cancelar";
+$this->idioma ["botonContinuar"] = "Continuar";
+
+$this->idioma ["division1Titulo"] = "Seleccione Fecha Disponibilidad Presupuestal";
+
+?>
