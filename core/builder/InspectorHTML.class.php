e760509e021c7dfb1ed37387f6e46077fc6f4726
stiv <stiv@localhost>
2015-06-06 17:52:27 -0500

    Modificaciones Core - Omitir limpiar etiquestas html, php y sql cuando Array sea en formato JSON

--- core/builder/InspectorHTML.class.php
+++ core/builder/InspectorHTML.class.php
@@ -1,60 +1,49 @@
 <?php
-
 class InspectorHTML {
-    
-    private static $instance;
-    
-    // Constructor
-    function __construct() {
-    
-    }
-    
-    public static function singleton() {
-        
-        if (! isset ( self::$instance )) {
-            $className = __CLASS__;
-            self::$instance = new $className ();
-        }
-        return self::$instance;
-    
-    }
-    
-    function limpiarPHPHTML($arreglo, $excluir = "") {
-        
-        if ($excluir != "") {
-            $variables = explode ( "|", $excluir );
-        } else {
-            $variables [0] = "";
-        }
-        
-        foreach ( $arreglo as $clave => $valor ) {
-            if (! in_array ( $clave, $variables )) {
-                
-                $arreglo [$clave] = strip_tags ( $valor );
-            }
-        }
-        
-        return $arreglo;
-    
-    }
-    
-    function limpiarSQL($arreglo, $excluir = "") {
-        
-        if ($excluir != "") {
-            $variables = explode ( "|", $excluir );
-        } else {
-            $variables [0] = "";
-        }
-        
-        foreach ( $arreglo as $clave => $valor ) {
-            if (! in_array ( $clave, $variables )) {
-                
-                $arreglo [$clave] = addcslashes ( $valor, '%_' );
-            }
-        }
-        
-        return $arreglo;
-    
-    }
-
+	private static $instance;
+	
+	// Constructor
+	function __construct() {
+	}
+	public static function singleton() {
+		if (! isset ( self::$instance )) {
+			$className = __CLASS__;
+			self::$instance = new $className ();
+		}
+		return self::$instance;
+	}
+	function limpiarPHPHTML($arreglo, $excluir = "") {
+		if ($excluir != "") {
+			$variables = explode ( "|", $excluir );
+		} else {
+			$variables [0] = "";
+		}
+		
+		foreach ( $arreglo as $clave => $valor ) {
+			if (! in_array ( $clave, $variables )) {
+				if (! is_array ( $arreglo )) {
+					$arreglo [$clave] = strip_tags ( $valor );
+				}
+			}
+		}
+		
+		return $arreglo;
+	}
+	function limpiarSQL($arreglo, $excluir = "") {
+		if ($excluir != "") {
+			$variables = explode ( "|", $excluir );
+		} else {
+			$variables [0] = "";
+		}
+		
+		foreach ( $arreglo as $clave => $valor ) {
+			if (! in_array ( $clave, $variables )) {
+				if (! is_array ( $arreglo )) {
+					$arreglo [$clave] = addcslashes ( $valor, '%_' );
+				}
+			}
+		}
+		
+		return $arreglo;
+	}
 }
