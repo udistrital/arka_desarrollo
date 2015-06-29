ace4ff7b2b5d3b5f3d60139a9f5f9501367d43f5
violetas <violetas@s242028c7g.udistritaloas.edu.co>
2015-06-29 02:45:55 -0500

    Reversa Contable iniciando

---
+++ blocks/inventarios/reversaContable/locale/es_es/Mensaje.php
@@ -0,0 +1,23 @@
+<?php
+$this->idioma ["noDefinido"] = "Etiqueta no esta definida en locale/es_es/Mensaje.php";
+
+
+$this->idioma ["fecha_inicio"] ="&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspFecha Inicial: ";
+$this->idioma ["fecha_inicioTitulo"] ="Seleccione Fecha Inicial";
+$this->idioma ["fecha_registro"] ="&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspFecha Registro: ";
+$this->idioma ["fecha_registroTitulo"] ="";
+$this->idioma ["fecha_final"] ="&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspFecha Final: ";
+$this->idioma ["fecha_finalTitulo"] ="Seleccione Fecha Final";
+$this->idioma ["vigencia"] ="&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspVigencia: ";
+$this->idioma ["vigenciaTitulo"] ="Seleccione Vigencia";
+$this->idioma ["aprobacion"] ="&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspAprobación: ";
+$this->idioma ["aprobacionTitulo"] ="¿Aprueba el Cierre Contable para el periodo Seleccionado?";
+$this->idioma ["observaciones"] ="&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspObservaciones";
+$this->idioma ["observacionesTitulo"] ="";
+
+
+$this->idioma ["botonAceptar"] ="Registrar Cierre Contable";
+$this->idioma ["botonCancelar"] ="Cancelar";
+$this->idioma ["botonContinuar"] ="Continuar";
+$this->idioma ["botonConsultar"] ="Consultar";
+$this->idioma ["botonRegresar"] ="Regresar";
